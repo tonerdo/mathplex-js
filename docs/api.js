1cd73d476e66eeab5b5262c71d9d294004e4d79b
Developer Without Borders <toni.edward@outlook.com>
2015-04-16 19:35:50 +0100

    Seventh commit. YUI docs generated

---
+++ docs/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Complex"
+    ],
+    "modules": [
+        "Mathplex"
+    ],
+    "allModules": [
+        {
+            "displayName": "Mathplex",
+            "name": "Mathplex",
+            "description": "This module contains the class and all methods for working with complex numbers"
+        }
+    ]
+} };
+});
