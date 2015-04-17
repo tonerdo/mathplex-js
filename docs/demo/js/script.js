f61b3c6cad38aa131d717cc84e21806da96cc202
Tonerdo <toni.edward@outlook.com>
2015-04-17 12:01:06 +0100

    Update script.js

--- docs/demo/js/script.js
+++ docs/demo/js/script.js
@@ -4,7 +4,7 @@ var second = document.querySelectorAll('#second')[0];
 var result = document.querySelectorAll('#result')[0];
 var valid = ['0','1','2','3','4','5','6','7','8','9','+','-','.'];
 var operators = document.querySelectorAll('#keypad button');
-var focused;
+var lastFocused;
 
 first.addEventListener('keyup', handleKeyUp);
 first.addEventListener('focus', handleFocus);
@@ -32,7 +32,7 @@ function handleKeyUp(e) {
 
 function handleFocus(e) {
   var element = e.srcElement;
-  focused = element;
+  lastFocused = element;
   var value = element.value;
   if (value == '0')
     element.value = '';
@@ -40,7 +40,6 @@ function handleFocus(e) {
 
 function handleBlur(e) {
   var element = e.srcElement;
-  focused = undefined;
   var value = element.value;
   if (value == '')
     element.value = '0';
@@ -53,7 +52,7 @@ function handleOperation(e) {
   var c2 = parseComplex(second.value);
   var use = element.getAttribute('data-use');
 
-  var c3 = new Complex(0,0);
+  var c3 = 0;
 
   if (use == 'add')
     c3 = c1.add(c2);
@@ -63,6 +62,30 @@ function handleOperation(e) {
     c3 = c1.multiply(c2);
   else if (use == 'div')
     c3 = c1.divide(c2);
+  else if (use == 'exp')
+    lastFocused.value = Complex.E.toString();
+  else if (use == 'pi')
+    lastFocused.value = Complex.PI.toString();
+  else if (use == 'sin')
+    lastFocused.value = Complex.sin(lastFocused.value);
+  else if (use == 'cos')
+    lastFocused.value = Complex.cos(lastFocused.value);
+  else if (use == 'tan')
+    lastFocused.value = Complex.tan(lastFocused.value);
+  else if (use == 'asin')
+    lastFocused.value = Complex.asin(lastFocused.value);
+  else if (use == 'acos')
+    lastFocused.value = Complex.acos(lastFocused.value);
+  else if (use == 'atan')
+    lastFocused.value = Complex.atan(lastFocused.value);
+  else if (use == 'log')
+    lastFocused.value = Complex.log(lastFocused.value);
+  else if (use == 'min')
+    c3 = Complex.min(c1, c2);
+  else if (use == 'max')
+    c3 = Complex.max(c1, c2);
+  else if (use == 'pow')
+    c3 = c1.pow(second.value);
 
   result.value = c3.toString();
 
